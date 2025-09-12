7a38eb1df4ae08a85e27470ca6cae9dde29debd9
Oskar Niesen <oskarniesen1@gmail.com>
2025-09-12 09:41:05 -0500

    Fixed bases updating all players instead just the one they are assigned to

--- game/src/base.cpp
+++ game/src/base.cpp
@@ -14,51 +14,47 @@ Base::Base(Game_Manager& game_manager, Card_Player& card_player, Vector2 pos, Pa
 }
 
 void Base::Update() {
-    if (time_until_income-- > 0)
-        return;
+    if (--time_until_income == 0) {
+        card_player.money++;
+        time_until_income = 40;
+    }
 
-    for (auto* player : game_manager.players) {
-        Card_Player* card_player = static_cast<Card_Player*>(player);
-        if (card_player->team == -1)
-            continue;
+    if (card_player.deck->hand.empty()) {
+        card_player.deck->Draw_Card(3);
+    }
 
-        card_player->money++;
-        if (card_player->deck->hand.empty()) {
-            card_player->deck->Draw_Card(3);
-        }
-
-        if (card_player->ai && !card_player->deck->hand.empty()) {
-            std::uniform_int_distribution<int> hand_dist(0, card_player->deck->hand.size() - 1);
-            int card_to_play = hand_dist(game_manager.random);
-            Card* card = card_player->deck->hand[card_to_play];
-            if (Tower_Card* tower_card = dynamic_cast<Tower_Card*>(card)) {
-                for (auto pos : path->positions) {
-                    static uniform_int_distribution<int> tiny_offset(-20, 20);
-                    Vector2 r_pos = Vector2(pos.x + 50 + abs(tiny_offset(game_manager.random)),
-                                            pos.y + tiny_offset(game_manager.random));
-                    if (tower_card->Can_Play_Card(card_player, r_pos)) {
-                        tower_card->Play_Card(card_player, r_pos);
-                        break;
-                    }
-                    Vector2 l_pos = Vector2(pos.x - 50 - abs(tiny_offset(game_manager.random)),
-                                            pos.y + tiny_offset(game_manager.random));
-                    if (tower_card->Can_Play_Card(card_player, l_pos)) {
-                        tower_card->Play_Card(card_player, l_pos);
-                        break;
-                    }
+    if (card_player.ai && !card_player.deck->hand.empty()) {
+        std::uniform_int_distribution<int> hand_dist(0, card_player.deck->hand.size() - 1);
+        int card_to_play = hand_dist(game_manager.random);
+        Card* card = card_player.deck->hand[card_to_play];
+        if (Tower_Card* tower_card = dynamic_cast<Tower_Card*>(card)) {
+            int i = 0;
+            for (auto pos : path->positions) {
+                i++;
+                static uniform_int_distribution<int> tiny_offset(-20, 20);
+                Vector2 r_pos = Vector2(pos.x + 50 + abs(tiny_offset(game_manager.random)),
+                                        pos.y + tiny_offset(game_manager.random));
+                if (tower_card->Can_Play_Card(&card_player, r_pos)) {
+                    tower_card->Play_Card(&card_player, r_pos);
+                    break;
                 }
-
-                // If we couldn't play the card due to lack of open positions discard it
-                if (card_player->deck->hand.size() > card_to_play &&
-                    card_player->deck->hand[card_to_play] == card &&
-                    card_player->money >= card->card_data.cost) {
-                    tower_card->Discard_Card(card_player);
+                Vector2 l_pos = Vector2(pos.x - 50 - abs(tiny_offset(game_manager.random)),
+                                        pos.y + tiny_offset(game_manager.random));
+                if (tower_card->Can_Play_Card(&card_player, l_pos)) {
+                    tower_card->Play_Card(&card_player, l_pos);
+                    break;
                 }
-
-            } else if (card->Can_Play_Card(card_player, Vector2Zero())) {
-                card->Play_Card(card_player, Vector2Zero());
             }
+
+            // If we couldn't play the card due to lack of open positions discard it
+            if (card_player.deck->hand.size() > card_to_play &&
+                card_player.deck->hand[card_to_play] == card &&
+                card_player.money >= card->card_data.cost) {
+                tower_card->Discard_Card(&card_player);
+            }
+
+        } else if (card->Can_Play_Card(&card_player, Vector2Zero())) {
+            card->Play_Card(&card_player, Vector2Zero());
         }
     }
-    time_until_income = 40;
 }
